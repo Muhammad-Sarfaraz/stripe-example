9c4aa0fffeecc3064aee6ce6b2dc40c19d650e20
id.sent.me@gmail.com <id.sent.me@gmail.com>
2022-04-19 02:47:11 +0600

    initial commit

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Charge;
+use Stripe\Stripe;
+use Stripe\Subscription;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function makePayment(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+        $charge = Charge::create([
+            "amount" => 120 * 100,
+            "currency" => "inr",
+            "source" => $request->stripeToken,
+            "description" => "Make payment and chill."
+        ]);
+
+        if ($charge['status'] == 'succeeded') {
+            return "Payment recived successfully!";
+        }
+
+        return "Please try again!";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
